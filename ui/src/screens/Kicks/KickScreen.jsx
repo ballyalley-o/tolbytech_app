06bcaade61869dd430f5eb37550f771f1078ed24
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-06-22 02:25:14 +1200

    mercator implemented

--- ui/src/screens/Kicks/KickScreen.jsx
+++ ui/src/screens/Kicks/KickScreen.jsx
@@ -7,6 +7,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Container, Typography, Grid } from '@mui/material'
 import { styled } from '@mui/material/styles'
 import Message from '../../components/Message'
+import Mercator from '../../components/graphs/Mercator/Mercator'
 
 const LinkBase = styled(Link)(({ theme }) => ({
   textDecoration: 'none',
@@ -50,7 +51,43 @@ const KickScreen = () => {
               </Message>
             ) : (
               <>
-                <h1>Kicks here</h1>
+                <Mercator
+                  title="Kicks"
+                  description="Kicks"
+                  width={1000}
+                  height={500}
+                  data={[
+                    { name: 'Nike', value: 100 },
+                    { name: 'Adidas', value: 100 },
+                    { name: 'Puma', value: 100 },
+                    { name: 'Reebok', value: 100 },
+                    { name: 'New Balance', value: 100 },
+                    { name: 'Vans', value: 100 },
+                    { name: 'Converse', value: 100 },
+                    { name: 'Jordan', value: 100 },
+                    { name: 'Asics', value: 100 },
+                    { name: 'Fila', value: 100 },
+                    { name: 'Saucony', value: 100 },
+                    { name: 'Under Armour', value: 100 },
+                    { name: 'Timberland', value: 100 },
+                    { name: 'Balenciaga', value: 100 },
+                    { name: 'Yeezy', value: 100 },
+                    { name: 'Off-White', value: 100 },
+                    { name: 'Gucci', value: 100 },
+                    { name: 'Louis Vuitton', value: 100 },
+                    { name: 'Dior', value: 100 },
+                    { name: 'Versace', value: 100 },
+                    { name: 'Prada', value: 100 },
+                    { name: 'Fendi', value: 100 },
+                    { name: 'Givenchy', value: 100 },
+                    { name: 'Chanel', value: 100 },
+                    { name: 'Balmain', value: 100 },
+                    { name: 'Valentino', value: 100 },
+                    { name: 'Saint Laurent', value: 100 },
+                    { name: 'Alexander McQueen', value: 100 },
+                    { name: 'Burberry', value: 100 },
+                  ]}
+                />
               </>
             )}
           </Grid>
