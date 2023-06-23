7a92e4ead8c91fe84b3ac60569645c10f5f15b46
bally <108769285+ballyalley-o@users.noreply.github.com>
2023-06-24 00:07:14 +1200

    updated readme

--- ui/src/screens/Products/admin/AllProductsScreen.jsx
+++ ui/src/screens/Products/admin/AllProductsScreen.jsx
@@ -2,12 +2,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { Helmet } from 'react-helmet-async'
-import { Typography, Grid, Button, Table, TableRow } from '@mui/material'
 import { useSelector } from 'react-redux'
+import { useGetProductsQuery } from '../../../slices/products-slice.js'
+import { Typography, Grid, Button, Table, TableRow } from '@mui/material'
+import DeleteIcon from '@mui/icons-material/Delete'
+import ModeEditIcon from '@mui/icons-material/ModeEdit'
+import Message from '../../../components/Message'
+import Loader from '../../../components/Loader'
 import { CLIENT } from '../../../constants'
 
 const AllProductsScreen = () => {
-  return <div>AllProductsScreen</div>
+  const { data: products, isLoading, error } = useGetProductsQuery()
+  console.log(products)
+  return (
+    <>
+      <Helmet>
+        <title>Admin | Products</title>
+      </Helmet>
+      <Grid container direction="row">
+        <Grid item md={12}>
+          <Grid container direction="column">
+            <Typography>Hey</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </>
+  )
 }
 
 export default AllProductsScreen
