16cddd2a19434c0ad61ddc08862e8d879c827f98
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-19 01:37:08 +1000

    refactor: wrap AllOrdersScreen content in Container for improved layout

--- ui/src/screens/Orders/admin/AllOrdersScreen.jsx
+++ ui/src/screens/Orders/admin/AllOrdersScreen.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Meta from '../../../components/Meta/Meta'
 import { useGetOrdersQuery } from '../../../slices/order-slice'
 import { useUserQuery } from '../../../slices/user-slice'
-import { Grid, Typography, TableBody } from '@mui/material'
+import { Container, TableBody } from '@mui/material'
 import {
   TableBase,
   TableHeadBase,
@@ -30,49 +30,51 @@ const AllOrdersScreen = () => {
   return (
     <>
       <Meta title={MetaTitles.ADMIN_ORDERS} />
-      <AdminHeading title="Orders" />
-      {isLoading ? (
-        <Loader />
-      ) : error ? (
-        <Message variant="h3" pr={3} py={3} fontWeight="bold">
-          No recent orders &nbsp; <LinkBase href="/">Go Back</LinkBase>
-        </Message>
-      ) : (
-        <>
-          <TableContainerBase>
-            <TableBase>
-              <TableHeadBase>
-                <TableRowHeaderBase color={theme.palette.primary.main}>
-                  <TableCellBase align="center"></TableCellBase>
-                  <TableCellBase align="left">CUSTOMER</TableCellBase>
-                  <TableCellBase align="left">DATE</TableCellBase>
-                  <TableCellBase align="center">ORDER NO</TableCellBase>
-                  <TableCellBase align="center">AMOUNT</TableCellBase>
-                  <TableCellBase align="center">PAID</TableCellBase>
-                  <TableCellBase align="center">DELIVERED</TableCellBase>
-                </TableRowHeaderBase>
-              </TableHeadBase>
-              <TableBody>
-                {orders.response.map((order) => (
-                  <Row
-                    key={order._id}
-                    row={order}
-                    customer
-                    content={
-                      <OrderViewAccounts
-                        order={order}
-                        error={error}
-                        user={buyer?.response}
-                        adminOrder
-                      />
-                    }
-                  />
-                ))}
-              </TableBody>
-            </TableBase>
-          </TableContainerBase>
-        </>
-      )}
+      <Container>
+        <AdminHeading title="Orders" />
+        {isLoading ? (
+          <Loader />
+        ) : error ? (
+          <Message variant="h3" pr={3} py={3} fontWeight="bold">
+            No recent orders &nbsp; <LinkBase href="/">Go Back</LinkBase>
+          </Message>
+        ) : (
+          <>
+            <TableContainerBase>
+              <TableBase>
+                <TableHeadBase>
+                  <TableRowHeaderBase color={theme.palette.primary.main}>
+                    <TableCellBase align="center"></TableCellBase>
+                    <TableCellBase align="left">CUSTOMER</TableCellBase>
+                    <TableCellBase align="left">DATE</TableCellBase>
+                    <TableCellBase align="center">ORDER NO</TableCellBase>
+                    <TableCellBase align="center">AMOUNT</TableCellBase>
+                    <TableCellBase align="center">PAID</TableCellBase>
+                    <TableCellBase align="center">DELIVERED</TableCellBase>
+                  </TableRowHeaderBase>
+                </TableHeadBase>
+                <TableBody>
+                  {orders.response.map((order) => (
+                    <Row
+                      key={order._id}
+                      row={order}
+                      customer
+                      content={
+                        <OrderViewAccounts
+                          order={order}
+                          error={error}
+                          user={buyer?.response}
+                          adminOrder
+                        />
+                      }
+                    />
+                  ))}
+                </TableBody>
+              </TableBase>
+            </TableContainerBase>
+          </>
+        )}
+      </Container>
     </>
   )
 }
