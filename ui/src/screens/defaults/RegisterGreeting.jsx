71b96f86176be85e3233b53f9126252ef206db4f
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-19 10:25:45 +1000

    refactor: update RegisterGreeting component for improved layout and styling

--- ui/src/screens/defaults/RegisterGreeting.jsx
+++ ui/src/screens/defaults/RegisterGreeting.jsx
@@ -2,55 +2,21 @@
 import React from 'react'
 import { Grid, Typography, CardMedia, Container } from '@mui/material'
 import { styled } from '@mui/material/styles'
-import bg from '../../assets/images/bg.png'
 import bg2 from '../../assets/images/bg2.png'
 
 const RegisterGreeting = () => {
   const ContainerBase = styled(Container)(({ theme }) => ({
-    color: theme.palette.common.black,
+    color : theme.palette.common.black,
+    mt: '50px'
   }))
   return (
     <>
-      <ContainerBase maxWidth="lg" maxHeight="sm">
+      <ContainerBase maxWidth="lg" maxHeight="md">
         <Grid container direction="row" justifyContent="center">
           <Grid item lg={12}>
             <Typography variant="h1" fontWeight="bold">
-              Join. Tolby Tech
+              Join. Tolby Tech.
             </Typography>
-            <CardMedia
-              component="img"
-              height="140"
-              image={bg}
-              alt="Tech"
-              sx={{
-                zIndex: -1,
-                position: 'absolute',
-                objectFit: 'fill',
-                float: 'left',
-                right: 0,
-                top: 0,
-                width: 'auto',
-                height: '100%',
-                // center the image
-              }}
-            />
-            <CardMedia
-              component="img"
-              height="140"
-              image={bg2}
-              alt="Tech"
-              sx={{
-                zIndex: -2,
-                position: 'absolute',
-                objectFit: 'fill',
-                float: 'left',
-                left: 0,
-                top: 0,
-                width: 'auto',
-                height: '100vh',
-                // center the image
-              }}
-            />
           </Grid>
         </Grid>
       </ContainerBase>
