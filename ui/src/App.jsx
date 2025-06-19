dc7912c6c37d645cf9a53c2953a5b1a96bf0251a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-19 10:27:53 +1000

    refactor: simplify App component imports and container styling

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
 import { ThemeProvider } from '@mui/material/styles'
-import { Typography, Container, Snackbar, Alert } from '@mui/material'
-import TolbyNavBar from './components/NavBar/TolbyNavBar'
-import Footer from './components/Footer'
+import { Container } from '@mui/material'
 import { ToastContainer } from 'react-toastify'
 import { HelmetProvider } from 'react-helmet-async'
+import TolbyNavBar from './components/NavBar/TolbyNavBar'
+import Footer from './components/Footer'
 import theme from './themes/theme'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -16,7 +16,7 @@ const App = () => {
       <HelmetProvider>
         <ThemeProvider theme={theme}>
           <TolbyNavBar />
-            <Container sx={{ height: '100%', alignContent: 'center', margin: 'auto', padding: '50px' }}>
+            <Container>
             <Outlet />
             <Footer />
           </Container>
